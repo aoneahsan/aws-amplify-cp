13a78d438a39890c7e644edb78978f2ce60e4986
Ahsan Mahmood <aoneahsan@gmail.com>
2023-03-17 19:28:09 +0500

    v1 - working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "zaionsurlshortener": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "zaionsurlshortenerfbd18cd6fbd18cd6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "zaionsurlshorteners3cfd16e62": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
