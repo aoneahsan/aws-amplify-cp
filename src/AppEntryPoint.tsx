81a4b0411c3ec20079576cfc659a1aa3ae67f713
aoneahsan <aoneahsan@gmail.com>
2023-03-25 12:28:07 +0500

    recoil-cp - before moving to https://github.com/jacques-blom/recoil-course, taking backup

--- src/AppEntryPoint.tsx
+++ src/AppEntryPoint.tsx
@@ -1,4 +1,26 @@
 import AppHOCWrappers from './AppHOCWrappers';
+import { setupIonicReact } from '@ionic/react';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+setupIonicReact();
 
 // Use this to add global listeners etc (where state is not needed), if state is needed then use "ZaionsApp.tsx"
 const AppEntryPoint: React.FC = () => (
