9338ee3bc2c2049c9b855fd154c0978acf1ac668
aoneahsan <aoneahsan@gmail.com>
2023-04-03 07:35:45 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aws-amplify-cp-ionic7-react',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
