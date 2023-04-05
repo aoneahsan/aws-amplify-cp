ee735811508615beff15ef69b507d2aa57150b30
aoneahsan <aoneahsan@gmail.com>
2023-04-05 15:48:17 +0500

    working on leads pages

---
+++ src/pages/Dashboard/Leads/CreateLead/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ICreateLeadPageProps {
+  dummyProp_NOT_NEEDED___ADDED_FOR_DEMO?: string;
+}
+
+const CreateLeadPage: React.FC<ICreateLeadPageProps> = () => {
+  return <>CreateLeadPage</>;
+};
+
+export default CreateLeadPage;
---
+++ src/pages/Dashboard/Leads/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ILeadsListPageProps {
+  dummyProp_NOT_NEEDED___ADDED_FOR_DEMO?: string;
+}
+
+const LeadsListPage: React.FC<ILeadsListPageProps> = () => {
+  return <>LeadsListPage</>;
+};
+
+export default LeadsListPage;
